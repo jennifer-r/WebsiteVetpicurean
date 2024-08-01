16c5ccde7cb5a16904870d6e885c9e13ec1d9386
gaby <76735916+gabypaulina@users.noreply.github.com>
2024-08-01 12:26:34 +0700

    fix nav + add fonts

--- Vetpicurean/vite-project/src/App.jsx
+++ Vetpicurean/vite-project/src/App.jsx
@@ -5,33 +5,37 @@ import logo from './assets/vetpic.png'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 function App() {
 
   return (
     <>
       <Fragment>
-        <Navbar expand="lg" className='p-4'>
+        <Navbar expand="lg">
           <Container fluid>
-            <Navbar.Brand href="#" >
-              halo
+            {/* <Navbar.Brand href="#home" > */}
               <img src={logo} alt="" style={{width: "15%"}}/>
-            </Navbar.Brand>
+            {/* </Navbar.Brand> */}
             <Navbar.Toggle aria-controls="navbarScroll" />
-            <Navbar.Collapse id="navbarScroll">
+            <Navbar.Collapse id="navbarScroll" className='justify-content-center font-link'>
               <Nav
-                className="me-auto my-2 my-lg-0"
-                style={{ maxHeight: '100px' }}
+                className="my-2 my-lg-0"
+                style={{ maxHeight: '100px', fontWeight: 'bold'}}
                 navbarScroll
               >
-                <Nav.Link href="#action1">Home</Nav.Link>
-                <Nav.Link href="#action2">Link</Nav.Link>
-                
-                <Nav.Link href="#" disabled>
-                  Link
-                </Nav.Link>
+                <Nav.Link href="#action1">HOME</Nav.Link>
+                <Nav.Link href="#action2">ABOUT</Nav.Link>
+                <Nav.Link href="#action2">TIPS</Nav.Link>
+                <Nav.Link href="#action2">PRODUCTS</Nav.Link>
+                <Nav.Link href="#action2">CONTACT</Nav.Link>
               </Nav>
             </Navbar.Collapse>
+            <div className="d-flex">
+              <Button className='me-4' style={{borderColor: '#6BF584', color: '#6BF584', backgroundColor: '#FFFFFF', width: '110px', fontWeight: 'bold'}}>LOG IN</Button>
+              <Button style={{backgroundColor: '#6BF584', borderColor: '#6BF584', width: '110px', fontWeight: 'bold'}}>REGISTER</Button>
+            </div>
           </Container>
         </Navbar>
       </Fragment>
